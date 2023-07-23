480ed4231fa7c84a970ddf3812af60c9b4b0488c
onslaught7 <krutibashmohapatra7@gmail.com>
2023-07-23 17:52:02 +0530

    Initial Project Boost files

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float rotationThrust = 100f;
+    
+    Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // HorizontalWrapMode we store rigid body in the rb variable
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // another way of performing the same action using of x, y z info
+            // rb.AddRelativeForce(0, 1, 0);
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            // Debug.Log("Pressed Space - Thrusting");
+        }
+    }
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; // freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; 
+        // another way of performing the same action using of x, y z info
+        // rb.AddRelativeForce(0, 0, 1);
+    }
+}
